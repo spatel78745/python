903199ea8b44034476ae187a259660a44d38202f
Sameer Patel <spatel78745@gmail.com>
2015-11-19 17:59:47 -0500

    Added some more tk examples

--- f2m.py
+++ f2m.py
@@ -14,6 +14,7 @@ root = Tk()
 root.title("Feet to Meters")
 
 mainframe = ttk.Frame(root, padding = "3 3 12 12")
+# I think (a, b, c) is a tuple
 mainframe.grid(column = 0, row = 0, sticky = (N, W, E, S))
 mainframe.columnconfigure(0, weight = 1)
 mainframe.rowconfigure(0, weight = 1)
