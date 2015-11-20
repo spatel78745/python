d124c4b426c4704109b953269b0f9411a74be026
Sameer Patel <spatel78745@gmail.com>
2015-11-19 22:16:51 -0500

    some refactoring

--- tksetup.py
+++ tksetup.py
@@ -25,16 +25,30 @@ def circle(x, y, r):
 def rad(deg):
   return math.pi / 180 * deg
 
-def plotr(theta, x = 200, y = 200, r = 75):
+def xr(theta, x, y, r):
   theta = rad(theta)
-  xr = r * math.cos(theta) + x
-  yr = y - r * math.sin(theta)
-  circle(xr, yr, 5)
+  return r * math.cos(theta) + x
+
+def yr(theta, x, y, r):
+  theta = rad(theta)
+  return y - r * math.sin(theta)
+
+def plotr(theta, x = CANVAS_WIDTH / 2, y = CANVAS_WIDTH / 2, r = 75):
+#  theta = rad(theta)
+#  xr = r * math.cos(theta) + x
+#  yr = y - r * math.sin(theta)
+  circle(xr(theta, x, y, r), yr(theta, x, y, r), 5)
 
 def testPlotr():
   for theta in range(0, 360):
     plotr(theta)
 
-# testPlotr()
+#def node(theta = 30, radius = 50, x = CANVAS_WIDTH / 2, y = CANVAS_WIDTH / 2, len = 50):
+#  rc_theta = rad(-90 + 0.5 * theta)
+#  lc_theta = rad(-90 - (0.5 * theta))
+#  rc_x = radius * math.cos(rc_theta) + x;
+
+
+testPlotr()
 #circle(200, 200, 20)
 
